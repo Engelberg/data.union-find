7615d9b68fc13408fa21bc0fe077a939e5d4486a
Mark Engelberg <mark.engelberg@gmail.com>
2020-05-04 01:08:32 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject engelberg/data.union-find "1.0.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Union find data structure"
+  :url "https://github.com/Engelberg/data.union-find"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
